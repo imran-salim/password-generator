ef6e0a9a3eb7b3fa9af1a772895355590335141f
imran-salim <imransalim08@gmail.com>
2021-04-03 20:25:10 +1300

    document code and refactor string modules

--- password-generator.py
+++ password-generator.py
@@ -1,22 +1,27 @@
 from os import urandom
 from struct import unpack
-import string
+from string import punctuation as p
+from string import ascii_letters as l
+from string import digits as d
 
 terminated = False
 while not terminated:
     try:
         pw_len = int(input('Please specify a length between 8 and 80 inclusive for your desired password.\n\n'))
 
+        # Terminate the program if the password length provided is not between 8 and 80 inclusive
         if pw_len < 8 or pw_len > 80:
             print('\nThe length provided is not in between 8 and 80 inclusive.')
             terminated = True
             continue
 
-        punctuation = string.punctuation
-        letters = string.ascii_letters
-        digits = string.digits
+        # Collect a set of all possible valid password characters
+        punctuation = p
+        letters = l
+        digits = d
         random_chars = punctuation + letters + digits
 
+        # Generate a random password of the given length
         generated_pw = ''
         n = 0
         while n < pw_len:
